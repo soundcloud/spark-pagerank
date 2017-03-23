24f84b88190330a893a956d8584db6d956dc2f42
Josh Devins <josh@soundcloud.com>
2017-03-23 17:40:38 +0100

    Adds an RDD version of our PageRank
    This is necessary due to the persistent and unfixed bug in Spark:
    https://issues.apache.org/jira/browse/SPARK-5480

---
+++ src/main/scala/com.soundcloud.spark/PlainPageRank.scala
@@ -0,0 +1,235 @@
+package com.soundcloud.spark
+
+import scala.reflect.ClassTag
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+
+import com.soundcloud.spark.GraphUtils._
+
+object PlainPageRank {
+
+  type VertexId = Long
+  type VertexValue = Double
+  type EdgeValue = Double
+
+  type Edges = RDD[(VertexId, Edge)] // (srcId, Edge) -- optimal for joins
+  type Vertices = RDD[(VertexId, VertexMetadata)]
+
+  case class Edge(dstId: VertexId, weight: EdgeValue)
+  case class VertexMetadata(value: VertexValue, isDangling: Boolean) {
+    /**
+     * Copies the vertex properties but provides a new value. This creates a new
+     * immutable vertex instance.
+     */
+    def withNewValue(newValue: VertexValue): VertexMetadata =
+      VertexMetadata(newValue, isDangling)
+  }
+
+  val DefaultTeleportProb: VertexValue = 0.15
+  val DefaultMaxIterations: Int = 100
+
+  def buildVerticesFromValues[ED: ClassTag](
+      edges: Edges,
+      vertices: RDD[(VertexId, VertexValue)]): Vertices = {
+
+    val a = edges.map(_._1).distinct.map(x => (x, 1)) // srcId
+    val b = edges.map(_._2.dstId).distinct.map(x => (x, 1)) // dstId
+
+    val dangles = a
+      .cogroup(b)
+      .map { case(id, vals) =>
+        val isDangling = vals._1.size == 0 && vals._2.size == 1
+        (id, isDangling)
+      }
+
+    vertices
+      .join(dangles)
+      .map { case(id, (value, isDangling)) =>
+        (id, VertexMetadata(value, isDangling))
+      }
+  }
+
+  /**
+   * Runs PageRank using the RDD APIs of Spark. This supports weighted edges and
+   * "dangling" vertices (no out edges). For performance considerations, the
+   * {{edges}} and {{vertices}} must already be cached (ideally in-memory) and
+   * this will be verified at runtime. The vertices will be unpersisted and
+   * persisted again at the same {{StorageLevel}} as they are mutated after each
+   * iteration.
+   *
+   * The structural requirements of the input graph are not enforced at runtime
+   * but can be checking using supporting methods. To ensure a proper graph,
+   * it is also suggested to use the supporting graph building methods. In
+   * general, the requirements for the graph that we expect are:
+   *  - All vertex IDs referenced in `edges` exist in `vertices`
+   *  - Graph has no self-referencing nodes (i.e. edges where in and out nodes
+   *    are the same)
+   *  - Vertex values are normalized (i.e. sum to `1.0`)
+   *  - Edge weights are normalized (i.e. outgoing edge values sum to `1.0`)
+   *
+   * @param edges the edges of the graph
+   * @param vertices the vertices of the graph
+   * @param teleportProb probability of a random jump in the graph
+   * @param maxIterations a threshold on the maximum number of iterations,
+   *          irrespective of convergence
+   * @param convergenceThresholdOpt an optional threshold on the change between
+   *          iterations which marks convergence (NOTE: providing this will
+   *          cause an extra computation after each iteration, so if performance
+   *          is of concern, do not provide a value here)
+   * @param numVerticesOpt the number of vertices in the input graph calculated
+   *          lazily from {{vertices}} if none is provided here (requires one
+   *          extra job so this is an optimization if you already have this
+   *          value, for example from building vertices with a uniform value
+   *          distribution for a prior)
+   *
+   * @return the PageRank vector
+   */
+  def run(
+    edges: Edges,
+    vertices: Vertices,
+    teleportProb: VertexValue = DefaultTeleportProb,
+    maxIterations: Int = DefaultMaxIterations,
+    convergenceThresholdOpt: Option[VertexValue] = None,
+    numVerticesOpt: Option[VertexId] = None): Vertices = {
+
+    require(edges.getStorageLevel != StorageLevel.NONE, "Storage level of edges cannot be `NONE`")
+    require(vertices.getStorageLevel != StorageLevel.NONE, "Storage level of vertices cannot be `NONE`")
+
+    require(teleportProb >= 0.0, "Teleport probability must be greater than or equal to 0.0")
+    require(teleportProb < 1.0, "Teleport probability must be less than 1.0")
+    require(maxIterations >= 1, "Max iterations must be greater than or equal to 1")
+
+    convergenceThresholdOpt.map { t =>
+      require(t > 0.0, "Convergence threshold must be greater than 0.0")
+      require(t < 1.0, "Convergence threshold must less than 1.0")
+    }
+
+    numVerticesOpt.map { n =>
+      require(n >= 2, "Number of vertices must be greater than or equal to 2")
+    }
+
+    // lazily calculated when needed, constant over all iterations
+    val numVertices = numVerticesOpt.getOrElse(vertices.count())
+
+    // iterate until the maximum number of iterations or until convergence (optional)
+    var newVertices = vertices
+    var hasConverged = false
+    var numIterations = 0
+    while (!hasConverged && numIterations < maxIterations) {
+
+      // save the vertices before the iteration starts in order to check for
+      // convergence after the iteration
+      val previousVertices = newVertices
+
+      // perform a single PageRank iteration
+      // this will create new vertices
+      // persist the new vertices at the same level as the previous vertices
+      newVertices =
+        iterate(edges, previousVertices, teleportProb, numVertices)
+          .persist(previousVertices.getStorageLevel)
+
+      // check for convergence (if threshold was provided)
+      convergenceThresholdOpt.map { t =>
+        hasConverged = delta(previousVertices, newVertices) < t
+      }
+
+      // finished with the previousVertices, so unpersist them
+      previousVertices.unpersist()
+
+      numIterations += 1
+    }
+
+    // vertices from the last iteration performed
+    newVertices
+  }
+
+  /**
+   * A single PageRank iteration.
+   */
+  private[spark] def iterate(
+      edges: Edges,
+      vertices: Vertices,
+      teleportProb: VertexValue,
+      numVertices: VertexId): Vertices = {
+
+    /**
+     * Calculates the new PageRank value of a vertex given the incoming
+     * probability mass plus the teleport probability.
+     */
+    def calculateVertexUpdate(incomingSum: VertexValue): VertexValue =
+      ((1 - teleportProb) * incomingSum) + (teleportProb / numVertices)
+
+    /**
+     * Calculates the dangling node delta update, after the normal vertex
+     * update. Note the `n - 1` is to account for no self-references in this
+     * node we are updating.
+     */
+    def calculateDanglingVertexUpdate(startingValue: VertexValue): VertexValue =
+      (1 - teleportProb) * (1.0 / (numVertices - 1)) * startingValue
+
+    /**
+     * Closure that updates the PageRank value of a node based on the incoming
+     * sum/probability mass.
+     *
+     * If the vertex is a "dangling" (no out edges):
+     *  - It does not distribute all its PageRank mass on out edges,
+     *    only through the teleport
+     *  - Since we do not want self-references, subtract the old PageRank mass
+     *    from the new one because we will distribute the missing mass equally
+     *    among all nodes.
+     */
+    def updateVertex(
+        perVertexMissingMass: VertexValue,
+        vMeta: VertexMetadata,
+        incomingSumOpt: Option[VertexValue]): VertexMetadata = {
+
+      val incomingSum = incomingSumOpt.getOrElse(0.0)
+      val newValue = calculateVertexUpdate(incomingSum) + perVertexMissingMass
+
+      if (vMeta.isDangling)
+        vMeta.withNewValue(newValue - calculateDanglingVertexUpdate(vMeta.value))
+      else
+        vMeta.withNewValue(newValue)
+    }
+
+    // collect what *will be* the dangling mass after the update that follows
+    val danglingMass =
+      vertices.
+        filter(_._2.isDangling).
+        map(_._2.value).
+        sum()
+
+    // send weights along edges to destinations
+    val incomingSumPerVertex = edges
+      .join(vertices)
+      .map { case(_, (edge, vMeta)) =>
+        // sends a proportional amount of mass to the destination
+        (edge.dstId, vMeta.value * edge.weight)
+      }.
+      reduceByKey(_ + _)
+
+    // distribute missing mass from dangling nodes equally to all other nodes
+    val perVertexMissingMass = calculateDanglingVertexUpdate(danglingMass)
+
+    // update the vertices with the transferred mass, plus teleport, plus dangling mass (where applicable)
+    vertices
+      .leftOuterJoin(incomingSumPerVertex)
+      .map { case(vId, (vMeta, incomingSumOpt)) =>
+        (vId, updateVertex(perVertexMissingMass, vMeta, incomingSumOpt))
+      }
+  }
+
+  /**
+   * Calculates the per-component change/delta and sums over all components
+   * (norm) to determine the total change/delta between the two vectors.
+   */
+  private[spark] def delta(left: Vertices, right: Vertices): VertexValue = {
+    left.
+      join(right).
+      map { case (_, (l, r)) =>
+        math.abs(l.value - r.value)
+      }.
+      sum()
+  }
+}
---
+++ src/test/scala/com.soundcloud.spark/PlainPageRankTest.scala
@@ -0,0 +1,160 @@
+package com.soundcloud.spark
+
+import org.apache.spark.storage.StorageLevel
+import org.scalatest.{ FunSuite, Matchers }
+
+import com.soundcloud.spark.GraphUtils._
+import com.soundcloud.spark.test.SparkTesting
+
+class PlainPageRankTest
+  extends FunSuite
+  with Matchers
+  with GraphTesting
+  with SparkTesting {
+
+  test("PageRank: simple graph (1)") {
+    // https://github.com/purzelrakete/Pagerank.jl/blob/070a193826f6ff7c2fe11fb5556f21d039b82e26/test/data/simple.dot
+    // https://github.com/purzelrakete/Pagerank.jl/blob/070a193826f6ff7c2fe11fb5556f21d039b82e26/test/rank/simple.jl
+    val teleportProb = 0.5
+    val maxIterations = 100
+    val convergenceThreshold = Some(math.pow(10, -3))
+    val input = Seq(
+      (1, 2),
+      (3, 2),
+      (2, 1),
+      (2, 3)
+    )
+    val expected = Seq((1, 5.0/18), (2, 4.0/9), (3, 5.0/18))
+    execTest(input, expected, teleportProb, maxIterations, convergenceThreshold)
+  }
+
+  test("PageRank: simple graph (2)") {
+    // https://github.com/joshdevins/pagerank/blob/0eede643cefd2869be7be78b57afc372e67749fe/octave/pagerank.m#L204
+    val teleportProb = 0.15
+    val maxIterations = 100
+    val convergenceThreshold = Some(math.pow(10, -3))
+    val input = Seq(
+      (1, 5),
+      (2, 1),
+      (3, 1),
+      (4, 2),
+      (4, 3),
+      (5, 3),
+      (5, 4)
+    )
+    val expected = Seq((1, 0.284047), (2, 0.091672), (3, 0.207025), (4, 0.145353), (5, 0.271903))
+    execTest(input, expected, teleportProb, maxIterations, convergenceThreshold)
+  }
+
+  test("PageRank: graph with dangling node (1)") {
+    // https://github.com/joshdevins/pagerank/blob/0eede643cefd2869be7be78b57afc372e67749fe/octave/pagerank.m#L211
+    val teleportProb = 0.15
+    val maxIterations = 100
+    val convergenceThreshold = Some(math.pow(10, -5))
+    val input = Seq(
+      // 1 has no outgoing edges
+      (2, 1),
+      (3, 1),
+      (4, 2),
+      (4, 3),
+      (5, 3),
+      (5, 4)
+    )
+    val expected = Seq((1, 0.35766), (2, 0.17021), (3, 0.21517), (4, 0.15096), (5, 0.10600))
+    execTest(input, expected, teleportProb, maxIterations, convergenceThreshold)
+  }
+
+  test("PageRank: graph with dangling node (2)") {
+    val teleportProb = 0.15
+    val maxIterations = 100
+    val convergenceThreshold = Some(math.pow(10, -5))
+    val input = Seq(
+      (1, 2),
+      (1, 3),
+      (1, 5),
+      (2, 1),
+      (2, 5),
+      (3, 1),
+      (3, 4),
+      (3, 5),
+      (5, 1),
+      (5, 2),
+      (5, 3),
+      (6, 2)
+      // 4 has no outgoing edges
+      // 6 has no incoming edges
+    )
+    val expected = Seq((1,0.248500), (2,0.210849), (3,0.178671), (4,0.075625), (5,0.248500), (6,0.037856))
+    execTest(input, expected, teleportProb, maxIterations, convergenceThreshold)
+  }
+
+  test("PageRank: graph with two dangling nodes") {
+    val teleportProb = 0.15
+    val maxIterations = 100
+    val convergenceThreshold = Some(math.pow(10, -5))
+    val input = Seq(
+      (2, 1),
+      (2, 5),
+      (3, 1),
+      (3, 4),
+      (3, 5),
+      (5, 1),
+      (5, 2),
+      (5, 3),
+      (6, 2)
+      // 1 has no outgoing edges
+      // 4 has no outgoing edges
+      // 6 has no incoming edges
+    )
+    val expected = Seq((1, 0.2363), (2, 0.2149), (3, 0.1442), (4, 0.1060), (5, 0.2154), (6, 0.0832))
+    execTest(input, expected, teleportProb, maxIterations, convergenceThreshold)
+  }
+
+  private def execTest(
+    input: Seq[(Int, Int)],
+    expected: Seq[(Int, Double)],
+    teleportProb: Double,
+    maxIterations: Int,
+    convergenceThreshold: Option[Double]): Unit = {
+
+    val nodes = input.unzip
+    val n = (nodes._1 ++ nodes._2).distinct.size
+    val prior = 1.0 / n
+
+    val rdd = sc.parallelize(expandEdgeTuples(input))
+    val edges = normalizeOutEdgeWeightsRDD(rdd)
+      .map { e =>
+        (e.srcId, PlainPageRank.Edge(e.dstId, e.attr))
+      }
+      .persist(StorageLevel.MEMORY_ONLY)
+    val vertexValues = edges
+      .flatMap { case (srcId, edge) =>
+        Seq(srcId, edge.dstId)
+      }
+      .distinct()
+      .map((_, prior))
+    val vertices = PlainPageRank.buildVerticesFromValues(edges, vertexValues)
+      .persist(StorageLevel.MEMORY_ONLY)
+
+    val actual = PlainPageRank
+      .run(
+        edges,
+        vertices,
+        teleportProb,
+        maxIterations,
+        convergenceThreshold
+      )
+      .collect()
+
+    // error between each component, expected vs actual
+    def squaredError(actual: Seq[(Long, PlainPageRank.VertexMetadata)], expected: Seq[(Int, Double)]): Double = {
+      val expectedMap = expected.toMap
+      actual.map { case (id, vMeta) =>
+        math.pow(vMeta.value - expectedMap.getOrElse(id.toInt, 0.0), 2)
+      }.sum
+    }
+
+    actual.map(_._2.value).sum shouldBe 1.0 +- EPS
+    squaredError(actual, expected) shouldBe 0.0 +- math.pow(10, -5)
+  }
+}
