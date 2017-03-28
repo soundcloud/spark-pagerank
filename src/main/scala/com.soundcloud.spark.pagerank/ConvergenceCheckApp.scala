7bba444dc18ff695c9ef45008b03c1b3033973e3
Josh Devins <josh@soundcloud.com>
2017-03-28 17:45:41 +0200

    Adds an app to compare two PR vectors
    Use this to check after the fact for convergence.

---
+++ src/main/scala/com.soundcloud.spark.pagerank/ConvergenceCheckApp.scala
@@ -0,0 +1,47 @@
+package com.soundcloud.spark.pagerank
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+import org.kohsuke.args4j.{ CmdLineParser, Option => ArgOption }
+
+/**
+ * Compares two PageRank vectors and lets the user determine if there is
+ * convergence.
+ */
+object ConvergenceCheckApp extends SparkApp {
+  class Options {
+    @ArgOption(name = "--inputA", usage = "Version A of the PageRank vector", required = true)
+    var inputA: String = _
+
+    @ArgOption(name = "--inputB", usage = "Version B of the PageRank vector", required = true)
+    var inputB: String = _
+  }
+
+  def run(args: Array[String], sc: SparkContext): Unit = {
+    val options = new Options()
+    new CmdLineParser(options).parseArgument(args: _*)
+
+    val a = sc.objectFile[Vertex](s"${options.inputA}")
+    val b = sc.objectFile[Vertex](s"${options.inputB}")
+
+    val delta = sumOfDifferences(a, b)
+
+    println("Sum of the vertices: ")
+    println(f"  A: ${a.map(_.value).sum()}")
+    println(f"  B: ${b.map(_.value).sum()}")
+    println(f"Sum of component-wise differences: $delta%.15f")
+  }
+
+  private[pagerank] def sumOfDifferences(left: VertexRDD, right: VertexRDD): Value = {
+    val leftPair = left.map(v => (v.id, v.value))
+    val rightPair = right.map(v => (v.id, v.value))
+
+    leftPair
+      .join(rightPair)
+      .map { case (_, (l, r)) =>
+        math.abs(l - r)
+      }
+      .sum()
+  }
+}
